190a746b5902749dc4f0771742803ca111d8c0c5
Pedro de Andrade <dev.pedroandrade@gmail.com>
2023-10-10 22:01:44 -0300

    feat: Repository de users e controller com endpoint para criar usuários

--- src/main/java/com/pedroandrad/ToDoList/user/UserController.java
+++ src/main/java/com/pedroandrad/ToDoList/user/UserController.java
@@ -1,9 +1,28 @@
 package com.pedroandrad.ToDoList.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository repository;
+    @PostMapping()
+    public ResponseEntity createUser(@RequestBody UserModel user){
+        try {
+           UserModel createdUser = this.repository.save(user);
+           return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        }
+        catch (DataIntegrityViolationException err){
+            System.out.println(err);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 }
