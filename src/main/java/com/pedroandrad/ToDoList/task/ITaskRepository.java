f08938d972246c519cfd0db0c7b55cb0e7f31e61
Pedro de Andrade <dev.pedroandrade@gmail.com>
2023-10-12 20:36:15 -0300

    feat: Pegando tasks by User. Está executando duplicada.

--- src/main/java/com/pedroandrad/ToDoList/task/ITaskRepository.java
+++ src/main/java/com/pedroandrad/ToDoList/task/ITaskRepository.java
@@ -1,8 +1,11 @@
 package com.pedroandrad.ToDoList.task;
 
+import com.pedroandrad.ToDoList.user.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByUser(UserModel user);
 }
--- src/main/java/com/pedroandrad/ToDoList/task/TaskController.java
+++ src/main/java/com/pedroandrad/ToDoList/task/TaskController.java
@@ -6,10 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -21,7 +20,6 @@ public class TaskController {
 
     @PostMapping("/create") // Sei la pq isso so ta funcionando com @RequestBody(required = false)
     public ResponseEntity createTask(@RequestBody(required = false) TaskModel task, HttpServletRequest request){
-        System.out.println(task);
         try {
             UserModel user = userRepository.findByuserName(request.getAttribute("userName").toString());
             task.setUser(user);
@@ -31,7 +29,19 @@ public class TaskController {
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-
+    }
+    @GetMapping
+    public ResponseEntity getUserTasks(HttpServletRequest request){
+        System.out.println("DUPLICADO");
+        try {
+            UserModel user = userRepository.findByuserName(request.getAttribute("userName").toString());
+            List<TaskModel> tasks = taskRepository.findByUser(user);
+            System.out.println(tasks);
+            return  ResponseEntity.status(HttpStatus.OK).body(tasks);
+        }
+        catch (Exception e){
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
